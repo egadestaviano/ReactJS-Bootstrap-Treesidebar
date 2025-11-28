f3f6044241932494a2a9a282b4bf5aff4d5f428b
egadestaviano <pastilaku121@gmail.com>
2025-11-28 10:17:38 +0700

    Integrate breadcrumb navigation and tree view with tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Tabs, Tab } from "react-bootstrap";
 import "./App.css";
 import NavbarComponents from "./components/NavbarComponents";
 import Sidebar from "./components/Sidebar.jsx";
+import BreadcrumbNavigation from "./components/BreadcrumbNavigation.jsx";
+import TreeCheckbox from "./components/TreeCheckbox.jsx";
 
 function App() {
   return (
@@ -14,8 +16,16 @@ function App() {
           </Col>
           <Col xl={10} lg={9} className="ps-3">
             <div className="bg-body-tertiary p-3 rounded shadow-sm">
-              <h4>Content</h4>
-              {/* Tambahkan konten lainnya di sini */}
+              <BreadcrumbNavigation />
+              <Tabs defaultActiveKey="content" id="app-tabs" className="mb-3">
+                <Tab eventKey="content" title="Content">
+                  <h4>Main Content Area</h4>
+                  <p>This is the main content area where you can add your application content.</p>
+                </Tab>
+                <Tab eventKey="tree" title="Tree View">
+                  <TreeCheckbox />
+                </Tab>
+              </Tabs>
             </div>
           </Col>
         </Row>
